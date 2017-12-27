4c8ccda316ebff05ce82bae6b9d9e2403939e7b7
Jen Schumann <jenniferschumann1@gmail.com>
2017-12-27 12:01:50 -0800

    not doing many-to-many right now

--- models/climbers.js
+++ models/climbers.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 const Climb = require('./climbs.js');
-var Schema = mongoose.Schema,
-    ObjectId = Schema.ObjectId;
 
 const climberSchema = mongoose.Schema({
-        id: ObjectId,
         name: String,
         img: String,
         whatLoveREClimb14eenrs: String,
@@ -13,7 +10,6 @@ const climberSchema = mongoose.Schema({
         faveClimbGear: String,
         fourteenerClimbAdvice: String,
         wannaClimbThese14eenrs: String,
-        // climbs: [ {type : mongoose.Schema.ObjectId, ref : 'Climb'} ]
         climbs: [Climb.schema]
 });
 
